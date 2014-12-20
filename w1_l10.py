1d0bab9baeec21d934649c0919c61f6df0a683dc
EricSchles <ericschles@gmail.com>
2014-12-20 11:50:37 -0500

    level 10 done

---
+++ w1_l10.py
@@ -0,0 +1,14 @@
+# Your hero doesn't know these enemy's names!
+# The glasses give you the findNearestEnemy ability.
+
+enemy1 = self.findNearestEnemy()
+self.attack(enemy1)
+self.attack(enemy1)
+
+enemy2 = self.findNearestEnemy()
+self.attack(enemy2)
+self.attack(enemy2)
+
+enemy3 = self.findNearestEnemy()
+self.attack(enemy3)
+self.attack(enemy3)
